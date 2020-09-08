585d81535930f858d8d87c72532fb3be2d4acc44
Rachit Khattar <rachitkhattar01@gmail.com>
2020-09-08 13:24:00 +0530

    [x] Build error fix

--- rachit/Classes/Extensions/UIViewControllerExtension.swift
+++ rachit/Classes/Extensions/UIViewControllerExtension.swift
@@ -7,13 +7,4 @@ public extension UIViewController {
     static func loadFromNib<T: UIViewController>(_: T.Type) -> T {
         return T.init(nibName: String(describing: T.self), bundle: nil)
     }
-    
-    func displayAlert(withMessage alertMessage: String, withoutTitle: Bool = false) {
-        var title = ""
-        if !withoutTitle { title = Constants.ConstantStrings.AlertMessages.Title }
-        let alert = UIAlertController(title: title, message: alertMessage, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: Constants.ConstantStrings.AlertMessages.OptionOK, style: .default, handler: nil))
-        present(alert, animated: true, completion: nil)
-    }
-    
 }
--- rachit/Classes/Extensions/UIViewExtension.swift
+++ rachit/Classes/Extensions/UIViewExtension.swift
@@ -95,40 +95,6 @@ public extension UIView {
         }
     }
     
-    func hideAnimated(in stackView: UIStackView) {
-        if !self.isHidden {
-            UIView.animate(
-                withDuration: 0.35,
-                delay: 0,
-                usingSpringWithDamping: 0.9,
-                initialSpringVelocity: 1,
-                options: [],
-                animations: {
-                    self.isHidden = true
-                    stackView.layoutIfNeeded()
-            },
-                completion: nil
-            )
-        }
-    }
-    
-    func showAnimated(in stackView: UIStackView) {
-        if self.isHidden {
-            UIView.animate(
-                withDuration: 0.35,
-                delay: 0,
-                usingSpringWithDamping: 0.9,
-                initialSpringVelocity: 1,
-                options: [],
-                animations: {
-                    self.isHidden = false
-                    stackView.layoutIfNeeded()
-            },
-                completion: nil
-            )
-        }
-    }
-    
     func scaleIntoTopLeftCorner(){
         let positionAnimation = CABasicAnimation(keyPath: "bounds")
         positionAnimation.fromValue =  self.bounds
